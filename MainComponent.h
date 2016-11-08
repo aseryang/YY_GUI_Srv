8ac32beaf58ecfad0c94ff99c22b8b082d8a8d59
aseryang <464710970@qq.com>
2016-11-08 17:14:40 +0800

    Update MainComponent.h

--- MainComponent.h
+++ MainComponent.h
@@ -10,14 +10,15 @@
 #define MAINCOMPONENT_H_INCLUDED
 
 #include "../JuceLibraryCode/JuceHeader.h"
-
+#include "SPIniReadWrite.h"
+#include "PublicDef.h"
 
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainContentComponent   : public Component, public ListBoxModel
+class MainContentComponent   : public Component, public ListBoxModel, public ButtonListener
 {
 public:
     //==============================================================================
@@ -34,8 +35,28 @@ public:
 		Graphics& g,
 		int width, int height,
 		bool rowIsSelected);
+	void buttonClicked(Button* btnPressed);
+	void loadDataFromCfg();
 
 private:
+	String			parentPath;
+	CSPIniReadWrite sysCfg;
+	CSPIniReadWrite users;
+	CSPIniReadWrite userFriends;
+
+	TextButton* btnStart;
+	TextButton* btnStop;
+	TextPropertyComponent* ip;
+	TextPropertyComponent* tcpPort;
+	TextPropertyComponent* udpPort;
+	Label * connlab;
+	Label * conn;
+	Label * roomLab;
+	Label * roomNum;
+	ListBox* scrollBox;
+private:
+	friends			dataFriends;
+	userAndPassword dataNameAndPasswords;
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
 };
